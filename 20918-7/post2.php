c2a7b59718f720099d38afcab70f969266046e55
youngboy <121610435+youngjunboy@users.noreply.github.com>
2023-09-04 11:48:58 +0900

    Add files via upload

---
+++ 20918-7/post2.php
@@ -0,0 +1,6 @@
+<form action="post2_process.php" method="post">
+이름 : <input type="text" name="name"><br>
+전화번호 : <input type="text" name="tel"><br>
+주소 : <input type="text" name="address"><br>
+<input type="submit" value="저장">
+</form>
